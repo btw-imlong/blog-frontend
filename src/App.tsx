054385ef24b36ac95b6e87a74d56da298ab7189c
John Doe <johndoe@example.com>
2025-05-07 16:29:17 +0700

    create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Route, Routes } from "react-router";
-import Home from "./Pages/Home";
+// import { Route, Routes } from "react-router";
+// import Home from "./Pages/Home";
+
+import Navbar from "./Layout/Navbar";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+
+      </Routes> */}
+      <Navbar />
     </>
   );
 }
